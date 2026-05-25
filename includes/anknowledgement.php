cc47918aabde0930674603ab483a46e349c74aa3
ElliotCaval07 <106513795@student.swin.edu.au>
2026-05-25 12:03:13 +1000

    php files added
    Changed all previous files from HTML to PHP.
    
    Added all the includes files to the project (haven't been linked yet)

---
+++ includes/anknowledgement.php
@@ -0,0 +1,5 @@
+<p>
+    MediZen acknowledges the Wurundjeri people of the Kulin Nation as the
+    Traditional Owners of the land where we work and study. We pay our respects
+    to their Elders past, present, and emerging.
+</p>
---
+++ includes/navbar.php
@@ -0,0 +1,10 @@
+<header>
+    <img src="styles/images/MediZenLogo.jpeg" alt="MediZen logo" id="logo">
+    <h2>Where Health Comes To Rest</h2>
+    <nav>
+      <a href="index.php">Home Page</a>
+      <a href="apply.php">Job Application</a>
+      <a href="jobs.php">Job Information</a>
+      <a href="about.php">Background</a>
+    </nav>
+</header>
