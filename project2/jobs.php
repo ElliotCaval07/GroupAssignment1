7122f49d8516d32ea38da2f3968a7bcaa6b5854c
106523532 <106523532@student.swin.edu.au>
2026-05-31 12:33:30 +1000

    fixed jobs to how it was before someone (elliot ruined it (dont touch someone elses work before asking them))

--- project2/jobs.php
+++ project2/jobs.php
@@ -50,80 +50,45 @@
                 $responibilities = array_filter(explode("|", $row['responsibilities']));
                 $requirements = array_filter(explode("|", $row['requirements']));
             
-            }
+            
         ?>
 
-        <section class="job-pos">
-            <h3 class="centred">Digital wellness consultant</h3>
-            <h5>H5C7B</h5>
-            <p>
-                As the digital wellness consultant at mediZen you will act as the face of the company. This position will 
-                consist of supporting our clients with all of there health and wellness needs in a professional manner that 
-                builds trust in our brand. compassion and interest in helping the wellbeing of otheres is of the highest priority 
-                as it is important in assuring the integrity of out services.
-            </p>
-            <p>
-                <legend><h4>Resposibilities:</h4></legend> 
-                <ul>
-                    <li>Provide virtual consultations via video, chat, or phone</li>
-                    <li>Develop personalised wellness plans and goals</li>
-                    <li>Monitor client progress using digital tracking tools</li>
-                    <li>Ensure compliance with privacy and healthcare regulations</li>
-                </ul>
-            </p>
-            <p>
-                <legend><h4>requirments</h4></legend> 
-                <ul>
-                    <li>Strong communication and interpersonal skills</li>
-                    <li>Empathy and active listening</li>
-                    <li>Understanding of health and wellness principles</li>
-                </ul>
-            </p>
+       <section class="job-pos">
+                <h3 class="centred"><?php echo $row['name']; ?></h3>
+                <h5><?php echo $row['id']; ?></h5>
+                <p>
+                    <?php echo $row['description']; ?>
+                </p>
+                <p>
+                    <legend><h4>Resposibilities:</h4></legend> 
+                    <ul>
+                        <?php foreach ($responibilities as $entreeRes) {?>
+                            <li><?php echo $entreeRes; ?></li>
+                        <?php } ?>
+                    </ul>
+                </p>
+                <p>
+                    <legend><h4>requirments</h4></legend> 
+                    <ul>
+                        <?php foreach ($requirements as $entreeReq) {?>
+                            <li><?php echo $entreeReq; ?></li>
+                        <?php } ?>
+                    </ul>
+                </p>
 
                 <br>
 
                 <p><strong>Salary: </strong>$<?php echo $row['salary']; ?> monthly</p>
 
-                <p><strong>Reports to: </strong><?php echo $row['reports_to']; ?></p>                                    
-            
-            </section>
-            <br>
-
-        <section class="job-pos">
-            <h3 class="centred">Client data entry</h3>
-            <h5>JB235</h5>
-            <p>
-                A Client Data Entry Officer is responsible for accurately inputting, updating, and maintaining client 
-                information within digital systems. This role ensures that all records are complete, secure, and accessible 
-                to support efficient service delivery.
-            </p>
-            <p>
-                <legend><h4>Resposibilities:</h4></legend> 
-                <ul>
-                    <li>Enter client information into databases and digital systems</li>
-                    <li>Update and maintain accurate client records</li>
-                    <li>Organise and manage digital files and documents</li>
-                    <li>Identify and correct data errors</li>
-                </ul>
-            </p>
-            <p>
-                <legend><h4>requirments</h4></legend> 
-                <ul>
-                    <li>High attention to detail</li>
-                    <li>Fast and accurate typing skills</li>
-                    <li>Basic computer and data management skills</li>
-                </ul>
-            </p>
-
-            <br>
-
-            <p><strong>Salary: </strong>8,000 monthly</p>
-
-            <p><strong>Reports to: </strong>Client data Manager</p>
-            
+                <p><strong>Reports to: </strong><?php echo $row['reports_to']; ?></p>
         </section>
 
         <br>
+
+
+        <?php
+            }
+        ?>
         <style>
             .centred {text-align: center;}
 
